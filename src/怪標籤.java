3afda5e44ec4ab31f6c8c5dfe06e6feb69c99a04
謝勝全 <xoby12@gmail.com>
2016-06-23 19:32:51 +0800

    新增RO怪物資訊

--- src/怪標籤.java
+++ src/怪標籤.java
@@ -1,15 +1,19 @@
 
 public class 怪標籤 {
+	private int Lv;
 	private String 名稱;
-	private int 等級;
-	private int 血量;
-	private String 屬性;
+	private int HP;
+	private String Atk;
+	private double Base經驗;
+	private double Job經驗;
+	private String 種族;
 	private String 體型;
-	private double 經驗值;
-	private double 職業經驗值;
-	
-	
-	
-	
-	
+	private String 屬性;
+	private String Def;
+	private String MDef;
+	private String Hit100;
+	private String Flee95;
+	private String 攻擊距離;
+	private String 行為;
+	private double 擊殺次數;
 }
--- src/怪資訊.java
+++ src/怪資訊.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
 
 public class 怪資訊 {
+	public ArrayList<怪標籤> 怪資料庫 = new ArrayList<怪標籤>();
 	
+	怪資訊(){
+		
+	}
 }
